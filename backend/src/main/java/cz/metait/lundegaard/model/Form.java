15343192ca5fca65f9a6eda7722ade00984a4af7
Benjamin Varga <varga@metait.cz>
2019-03-18 16:49:29 +0100

    change to h2 and patterns

--- backend/src/main/java/cz/metait/lundegaard/model/Form.java
+++ backend/src/main/java/cz/metait/lundegaard/model/Form.java
@@ -2,7 +2,6 @@ package cz.metait.lundegaard.model;
 
 import lombok.Getter;
 import lombok.Setter;
-import lombok.extern.log4j.Log4j2;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -10,20 +9,25 @@ import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Pattern;
 
-@Log4j2
 @Getter
 @Setter
 @Entity
 @Table(name = "form")
 public class Form extends BaseEntity {
 
+  @Pattern(regexp = "^[A-Za-z0-9]+$")
   @Column(name = "policyNumber")
   private String policyNumber;
+  @Pattern(regexp = "^[A-Za-z]+$")
   @Column(name = "name")
   private String name;
+  @Pattern(regexp = "^[A-Za-z]+$")
   @Column(name = "surname")
   private String surname;
+  @Max(value = 2000)
   @Column(name = "request")
   private String request;
 
--- backend/src/main/java/cz/metait/lundegaard/model/RequestType.java
+++ backend/src/main/java/cz/metait/lundegaard/model/RequestType.java
@@ -2,13 +2,11 @@ package cz.metait.lundegaard.model;
 
 import lombok.Getter;
 import lombok.Setter;
-import lombok.extern.log4j.Log4j2;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
-@Log4j2
 @Setter
 @Getter
 @Entity
